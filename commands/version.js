8217279d17e6b014c69029ac416f5ccebae0ad35
Im2rnado <yassin.bedier@gmail.com>
2020-09-03 15:56:01 +0200

    updated v

--- commands/version.js
+++ commands/version.js
@@ -7,15 +7,14 @@ module.exports = {
 	aliases: ['v'],
 	execute(message) {
 		const yourEmbed = new Discord.MessageEmbed()
-			.setColor('#0099ff')
+			.setColor('#FA4454')
 			.setTitle('**Stinger Discord Bot**')
-			.setThumbnail('https://cdn.discordapp.com/avatars/743591965450305636/9288819898ed2298c59eba18d6de2ff8.webp')
+			.setThumbnail('https://cdn.discordapp.com/app-icons/743591965450305636/9288819898ed2298c59eba18d6de2ff8.png')
 			.addFields(
-				{ name: 'Current Bot Version', value: '2.0.0.0' },
+				{ name: 'Current Bot Version', value: '2.0' },
 				{ name: 'Discord.js Version', value: '12.3.1' },
 				{ name: 'Command Prefix', value: `${process.env.PREFIX}` },
-				{ name: 'Bot Developer', value: '[tornado](https://twitter.com/im2rnado)' },
-				{ name: 'Bot Host', value: 'Heroku' },
+				{ name: 'Owner/Developer', value: '[Tornado](https://twitter.com/im2rnado)' },
 				{ name: 'Support Server', value: 'https://discord.gg/CsHFZxh' })
 			.setTimestamp();
 
