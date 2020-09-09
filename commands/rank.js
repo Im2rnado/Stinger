6be1496b36bf1450536b510f3a14f1e33ad55ba7
im2rnado <im2rnado@gmail.com>
2020-09-09 22:16:09 +0000

    Create rank.js

---
+++ commands/rank.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+const Valorant = require('./login.js');
+const invite = ('https://discord.gg/CsHFZxh');
+
+module.exports = {
+	name: 'rank',
+	description: 'Returns your rank',
+	usage: '[username] [password]',
+	async execute(message) {
+
+		if (message.channel.type != 'dm') {
+			return message.channel.send('This command only works in DMs.');
+		}
+
+		const y = await message.channel.send('<a:loading:749963556316905494>  Getting your rank...'); (async () => {
+			try {
+				const rank = await valorant.getCompetitiveHistory();
+
+				const NewMessage = new Discord.MessageEmbed()
+					.setTitle('Balances')
+					.setColor('#FA4454')
+					.setThumbnail('https://www.m5.academy/img/valorant_logo.png')
+					.setFooter('Wrong info? Make sure to choose the correct region.')
+					.setDescription(`<:valorantp:745722786957492376> Valorant Rank: ${rank.res}\n<:radianitep:745722840782733445> Radianite Points: ${balance.RadianitePoints} ` );
+				return y.edit('', { embed: NewMessage });
+
+			}
+			catch (error) {
+				console.error(error.stack);
+				const errormessage1 = new Discord.MessageEmbed()
+					.setColor('#ffff00')
+					.setTitle('⚠️ **Uh  Oh! That was unexpected!**')
+					.setDescription(`An error has occurred and we're working on a fix ASAP!\nYou can [Join our Support Server](${invite}) and report it there.`)
+					.addField('Error Message: ', `\`\`\`js\n${error}\`\`\``);
+				return message.channel.send(errormessage1);
+			}
+		})();
+
+	},
+};
