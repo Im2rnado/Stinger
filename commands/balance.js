c3f144a988493d2e6adc672f67b969f0275a9db0
im2rnado <im2rnado@gmail.com>
2020-09-10 21:13:51 +0000

    Update balance.js

--- commands/balance.js
+++ commands/balance.js
@@ -6,7 +6,6 @@ module.exports = {
 	name: 'balance',
 	description: 'Returns your points',
 	aliases: ['bal', 'points'],
-	usage: '[username] [password]',
 	async execute(message) {
 
 		if (message.channel.type != 'dm') {
@@ -22,7 +21,7 @@ module.exports = {
 					.setColor('#FA4454')
 					.setThumbnail('https://www.m5.academy/img/valorant_logo.png')
 					.setFooter('Wrong info? Make sure to choose the correct region.')
-					.setDescription(`<:valorantp:745722786957492376> Valorant Points: ${balance.ValorantPoints}\n<:radianitep:745722840782733445> Radianite Points: ${balance.RadianitePoints} ` );
+					.setDescription(`<:valorantp:745722786957492376> Valorant Points: **${balance.ValorantPoints}**\n<:radianitep:745722840782733445> Radianite Points: **${balance.RadianitePoints}**`);
 				return y.edit('', { embed: NewMessage });
 
 			}
