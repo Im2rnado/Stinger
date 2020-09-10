7a07091459a3687e898e5302f8081984a272ad32
im2rnado <im2rnado@gmail.com>
2020-09-10 22:12:53 +0000

    Update login.js

--- commands/login.js
+++ commands/login.js
@@ -37,7 +37,7 @@ module.exports = {
 				const reaction = collected.first();
 
 				if (reaction.emoji.name === '1️⃣') {
-                                        message.reactions.removeAll();
+                                        h.reactions.removeAll();
 					const y = await message.channel.send('<a:loading:749963556316905494>  Signing in to Riot Services...'); (async () => {
 						try {
 
@@ -76,7 +76,7 @@ module.exports = {
 					})();
 				}
 				if (reaction.emoji.name === '2️⃣') {
-                                        message.reactions.removeAll();
+                                        h.reactions.removeAll();
 					const y = await message.channel.send('<a:loading:749963556316905494>  Signing in to Riot Services...'); (async () => {
 						try {
 
@@ -118,7 +118,7 @@ module.exports = {
 					})();
 				}
 				if (reaction.emoji.name === '3️⃣') {
-                                        message.reactions.removeAll();
+                                        h.reactions.removeAll();
 					const y = await message.channel.send('<a:loading:749963556316905494>  Signing in to Riot Services...'); (async () => {
 						try {
 
