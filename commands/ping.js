1d535ff9ef142d417770045aaa898bbd3c8e84dd
im2rnado <im2rnado@gmail.com>
2020-09-11 22:37:40 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,7 +4,7 @@ module.exports = {
 	async execute(message) {
 		// It sends the user "Pinging"
 		message.channel.send('Pinging...').then(m =>{
-m.edit(`**🏓 Pong!**\n**Latency is** ${m.createdTimestamp - message.createdTimestamp}ms.\n**API Latency is** ${Math.round(client.ws.ping)}ms`);
+m.edit(`**🏓 Pong!**\nLatency is \`${m.createdTimestamp - message.createdTimestamp}ms\`\nAPI Latency is \`${Math.round(client.ws.ping)}ms\``);
 })
 	},
 };2;
