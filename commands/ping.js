d1218e28f1b0fb5fdc38878a4639ba41ada19c88
im2rnado <im2rnado@gmail.com>
2020-09-11 22:30:25 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -5,6 +5,6 @@ module.exports = {
 		// It sends the user "Pinging"
 		message.channel.send('Pinging...').then(m =>{
 m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-}
+})
 	},
 };2;
