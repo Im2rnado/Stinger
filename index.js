83936e750bf1d2153c9d1558c545f5b8f6f81193
im2rnado <im2rnado@gmail.com>
2020-09-11 18:28:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const cooldowns = new Discord.Collection();
 
 client.once('ready', () => {
 	function randomStatus() {
-		const status = ['+help', 'v2.3.0'];
+		const status = ['+help', 'v2.3.2'];
 		const rstatus = Math.floor(Math.random() * status.length);
 
 		// client.user.setActivity(status[rstatus], {type: "WATCHING"});
