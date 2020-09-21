079885de14da4357b775b3cba1a39f52f26fd351
Im2rnado <yassin.bedier@gmail.com>
2020-09-21 15:41:10 +0200

    invite cmd

--- commands/invite.js
+++ commands/invite.js
@@ -2,12 +2,13 @@ const Discord = require('discord.js');
 
 module.exports = {
 	name: 'invite',
-	description: 'Invites you to the support server.',
+	description: 'Invites the bot to your server!',
 	execute(message) {
 		const yourEmbed = new Discord.MessageEmbed()
 			.setColor('#0099ff')
-			.setTitle('Join our support server')
-			.setDescription(':mailbox: [Press Me](https://discord.gg/CsHFZxh)');
+			.setTitle('Invite Stinger')
+			.setDescription(':mailbox: [Press Me](https://discord.com/api/oauth2/authorize?client_id=743591965450305636&permissions=8&scope=bot)')
+			.setFooter(`Requested By: ${message.author.tag}`);
 		return message.channel.send(yourEmbed);
 	},
 };
