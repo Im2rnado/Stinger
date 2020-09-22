a7ca5aacd1d1a3fb31ac366ecdcd4006599bc08b
im2rnado <yassin.bedier@gmail.com>
2020-09-22 16:04:24 +0200

    Update login.js

--- commands/login.js
+++ commands/login.js
@@ -151,7 +151,7 @@ module.exports = {
 				});
 		}
 		else {
-			message.channel.send('**You don't have premium! DM @tornado#9999 to buy Premium**');
+			message.channel.send('**You don\'t have premium! DM @tornado#9999 to buy Premium**');
 		}
 	},
 };
