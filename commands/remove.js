9cf99c667de0df6765f2aa93cda6e81d879473a1
im2rnado <yassin.bedier@gmail.com>
2020-09-22 13:48:43 +0200

    Update remove.js

--- commands/remove.js
+++ commands/remove.js
@@ -10,6 +10,7 @@ module.exports = {
 		if(message.author.id !== '510427790340915222') {
 			message.react('🤡');
 		}
+                else {
 
 		const target = message.mentions.members.first();
 		const user = message.mentions.users.first();
@@ -38,5 +39,6 @@ module.exports = {
 		else {
 			message.channel.send('Please specify who you want to remove from premium.');
 		}
+            }
 	},
 };
