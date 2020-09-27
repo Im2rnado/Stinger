0f32c9b7964c93ce5606bba54ae3aae8ce3e32a3
im2rnado <yassin.bedier@gmail.com>
2020-09-27 18:40:42 +0200

    better rate cmd

--- commands/rate.js
+++ commands/rate.js
@@ -1,17 +1,24 @@
+const Discord = require('discord.js');
+
 module.exports = {
 	name: 'rate',
 	description: 'Rates you!',
 	execute(message, args) {
 		const number = Math.floor(Math.random() * 101);
 		if (!args[0]) {
-			return message.channel.send('I would rate you a ' + number + '/100');
+			const emved = new Discord.MessageEmbed()
+				.setTitle('Your Rating')
+				.setColor('PINK')
+				.addField('I would rate you a', `:pleading_face: ${number}%`);
+			return message.channel.send(emved);
 		}
 		else {
 			const user = message.mentions.users.first();
-			if (!user) {
-				return message.channel.send('Please include who you are rating.');
-			}
-			return message.channel.send('I would rate ' + user.username + ' a ' + number + '/100');
+			const emved = new Discord.MessageEmbed()
+				.setTitle('Your Friend\'s Rating')
+				.setColor('PINK')
+				.addField(`I would rate ${user.username} a`, `:pleading_face: ${number}%`);
+			return message.channel.send(emved);
 
 		}
 
