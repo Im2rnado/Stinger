a58a8122ddbbc092d416d5831ef2804658a2d6ef
im2rnado <yassin.bedier@gmail.com>
2020-10-06 10:19:10 +0200

    fixed emoji numbers

--- commands/login.js
+++ commands/login.js
@@ -9,7 +9,7 @@ const yourEmbed = new Discord.MessageEmbed()
 const regionEmbed = new Discord.MessageEmbed()
 	.setColor('#FA4454')
 	.setTitle('**Please react with your region number.**')
-	.setDescription('<:eu:762949162194829313> Europe\n<:na:762949184839745557> North America\n3<:global:762949197833044020> Others');
+	.setDescription('<:eu:762949162194829313> Europe\n<:na:762949184839745557> North America\n<:global:762949197833044020> Others');
 
 module.exports = {
 	name: 'login',
@@ -32,17 +32,17 @@ module.exports = {
 			}
 
 			const h = await message.channel.send(regionEmbed);
-			h.react('<:eu:762949162194829313>').then(() => h.react('<:na:762949184839745557>')).then(() => h.react('<:global:762949197833044020>'));
+			h.react('762949162194829313').then(() => h.react('762949184839745557')).then(() => h.react('762949197833044020'));
 
 			const filter = (reaction, user) => {
-				return ['<:eu:762949162194829313>', '<:na:762949184839745557>', '<:global:762949197833044020>'].includes(reaction.emoji.name) && user.id === message.author.id;
+				return ['762949162194829313', '762949184839745557', '762949197833044020'].includes(reaction.emoji.name) && user.id === message.author.id;
 			};
 
 			h.awaitReactions(filter, { max: 1, time: 60000, errors: ['time'] })
 				.then(async collected => {
 					const reaction = collected.first();
 
-					if (reaction.emoji.name === '<:eu:762949162194829313>') {
+					if (reaction.emoji.name === '76294916219482931') {
 						await h.delete();
 						const y = await message.channel.send('<a:loading:749963556316905494>  Signing in to Riot Services...'); (async () => {
 							try {
@@ -76,7 +76,7 @@ module.exports = {
 							}
 						})();
 					}
-					if (reaction.emoji.name === '<:na:762949184839745557>') {
+					if (reaction.emoji.name === '762949184839745557') {
 						await h.delete();
 						const y = await message.channel.send('<a:loading:749963556316905494>  Signing in to Riot Services...'); (async () => {
 							try {
@@ -111,7 +111,7 @@ module.exports = {
 							}
 						})();
 					}
-					if (reaction.emoji.name === '<:global:762949197833044020>') {
+					if (reaction.emoji.name === '762949197833044020') {
 						await h.delete();
 						const y = await message.channel.send('<a:loading:749963556316905494>  Signing in to Riot Services...'); (async () => {
 							try {
